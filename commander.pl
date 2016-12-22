c183ebaec02a0d1b5ab3ae14cc2f2cfcf4d4c31a
lenny <lenny09918050@googlemail.com>
2016-12-22 03:19:56 +0100

    small updates - trying to bugfix

--- commander.pl
+++ commander.pl
@@ -1,34 +1,21 @@
 #!/usr/bin/env perl
 
-my $in  = *STDIN;
+my $in = *STDIN;
 my $out  = *STDOUT;
 
-if ($#ARGV >= 0){
+$out->print("My printing reaches the outside world\n");
 
-    unless (open($in,  "<", $ARGV[0])){
-      die "could not open $ARGV[0] for reading.";
-    }
-    
-    $out->print("So we managed to open the file: ");
-    $out->print($ARGV[0]);
-	$out->print("\n");
-	
-}
-
-while(true) {
-
-	$_ = <$in>;
+while($_ = <$in>) {
 	
 	$out->print("Received: ");
 	$out->print($_);
 	$out->print("\n");
 	
 
-
 	if($_ == "1") {		
 	    $out->print("I should run the command associated with 1 ;-) \n");
 	#    system("systemctl start someService.service")
-		$out->print("should've started someService.service\nW");
+		$out->print("should've started someService.service\n");
 	    
 	} elsif($_ == "2") {
 	    $out->print("I should run the command associated with 2 ;-) \n");
@@ -40,3 +27,5 @@ while(true) {
 	}
 
 }
+
+$out->print("And I'm gone!\n");
--- install.pl
+++ install.pl
@@ -9,4 +9,6 @@ system("sudo chmod 500 /root/commander.pl");
 
 system("sudo cp commander.service /etc/systemd/system/commander.service");
 system("sudo cp commander.socket /etc/systemd/system/commander.socket");
+
+system("sudo systemctl daemon-reload");
 system("sudo systemctl start commander.socket");
